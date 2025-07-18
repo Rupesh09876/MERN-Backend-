68b6f8999f02ea58ac631888602f2f73874c0ee3
Rupesh09876 <rupeshkatuwal53@gmail.com>
2025-07-18 11:47:48 +0545

    debug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { login } from "../controllers/authcontrollers.js";
 
-const router = express.Router();
+const router_auth = express.Router();
 
 // Route for user login
-router.post("/login", login);       
+router_auth.post("/login", login);       
 // Route for user registration (if needed, can be added later)
 
-export default router;
+export default router_auth;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import employeeRoutes from './employeeRoutes.js';
-import authRoutes from './authroutes.js';
+import authRoutes from './authRoutes.js';
 import departmentRoutes from './departmentRoutes.js';
 import leaveRoutes from './leaveroutes.js';
 import payrollRoutes from './payrollRoutes.js';
